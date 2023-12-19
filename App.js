2b249b2a91cb576dfeee8980e431279df6012c1d
Thanseehacp1 <taanzi0001@gmail.com>
2023-12-19 21:00:53 +0530

    project init

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StyleSheet, Text, View,FlatList } from 'react-native'
 import React from 'react'
-
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { AntDesign } from '@expo/vector-icons'; 
+import { Feather } from '@expo/vector-icons';
 const App = () => {
  
   const [activity1,setActivity1]=React.useState([
     {
       title:"coffee",
       item:"with Peter Bower",
-     
+     icon: <Feather name="coffee" size={24} color="white" />,
       rate:"$50",
     },
     
@@ -16,14 +18,16 @@ const App = () => {
     {
       title:"Gift",
       item:"for Audrey Campbell",
-      rate:"$50",
+      rate:"$1500",
+      icon: <AntDesign name="gift" size={24} color="white" />,
     },
     {
       title:"Subscription",
       item:"made with chapman",
-      rate:"$50",
+      rate:"$500",
+    icon:  <AntDesign name="mail" size={24} color="white" />
     },
-    
+  
   ]);
   const [activity2,setActivity2]=React.useState([
     {
@@ -31,11 +35,14 @@ const App = () => {
       item:"with Peter Bower",
     
       rate:"$50",
+      icon: <Feather name="coffee" size={24} color="white" />,
     },
     {
       title:"Subscription",
       item:"made with chapman",
-      rate:"$50",
+      rate:"$500",
+    
+      icon:  <AntDesign name="mail" size={24} color="white" />
     },
     
   ]);
@@ -107,40 +114,74 @@ const App = () => {
 data={activity1}
 
 renderItem={({item})=>(
-<View
-
-  style={{
-    //flexDirection:"row",
-    height:60,
-    width:290,
-    backgroundColor:"#e5e5e5",
-        marginTop:10,
-    borderRadius:20,
-    justifyContent:"center",
-    //alignItems:"center",
-    //marginLeft:10,
-  }}>
+  <View style={{
+ flex:1,
+  height:60,
+  width:290, 
+  backgroundColor:"#e5e5e5",
+      marginTop:10,
+  borderRadius:20,
+  justifyContent:"center",
+  //alignItems:"center",
+  //marginLeft:10,
+}}>          
+    <View style={{
+                 // flex: 1,
+                  backgroundColor: "pink",
+                  marginLeft: 4,
+                  alignSelf:"flexStart",
+                  alignContent:"center",
+                  alignItems:"center",
+                  justifyContent:"center",
+                    marginTop:30,
+                // marginBottom:1,
+                  width: 40,
+                  height: 30,
+                  borderRadius: 10,
+                }}
+              >
+  
+                  {item.icon}
+              
+                </View>  
+ 
   <Text style={{
     color:"black",
     fontSize:16,
     fontWeight:"bold",
     alignItems:"center",
-    marginLeft:40,
+    marginLeft:50,
+   // marginBottom:5,
   }}>
 {item.title}
 </Text>
 
-  
   <Text style={{
     color:"black",
     fontSize:12,
-    marginLeft:40,
+    marginLeft:50,
+    //marginBottom:8,
   }}>
 {item.item}
-  </Text>
-  
-  </View>
+ 
 
+   </Text>
+   <Text style={{
+  // marginRight:50,
+    //alignSelf:"flex-end",
+    color:"black",
+    fontSize:12,
+    marginBottom:40,
+   // marginRight:70,
+   // marginTop:30,
+    marginLeft:200,
+  }}>
+  {item.rate}
+</Text>
+   </View>
+ 
+ 
+ 
 )}
   />  
   
@@ -157,7 +198,8 @@ renderItem={({item})=>(
 <FlatList
 data={activity2}
 
-renderItem={({item,index})=>(
+renderItem={({item,icon})=>(
+
 <View
   style={{
     //flexDirection:"row",
@@ -170,24 +212,58 @@ renderItem={({item,index})=>(
     //alignItems:"center",
     //marginLeft:10,
   }}>
+  <View style={{
+                 // flex: 1,
+                  backgroundColor: "pink",
+                  marginLeft: 6,
+                  alignSelf:"flexStart",
+                  alignContent:"center",
+                  alignItems:"center",
+                  justifyContent:"center",
+                    marginTop:10,
+                // marginBottom:10,
+                  width: 40,
+                  height: 30,
+                  borderRadius: 10,
+                }}
+              >
+
+                  {item.icon}
+                  </View>      
   <Text style={{
     color:"black",
     fontSize:16,
     fontWeight:"bold",
     alignItems:"center",
     marginLeft:40,
+   // marginBottom:20,
   }}>
 {item.title}
 
   </Text>
+  
   <Text style={{
     color:"black",
     fontSize:12,
     marginLeft:40,
+    //marginBottom:40,
   }}>
 {item.item}
+
   </Text>
-  
+ 
+  <Text style={{
+  // marginRight:50,
+    //alignSelf:"flex-end",
+    color:"black",
+    fontSize:12,
+    marginBottom:40,
+   // marginRight:70,
+   // marginTop:30,
+    marginLeft:200,
+  }}>
+  {item.rate}
+</Text>
 </View>
 )}
   />  
